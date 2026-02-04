e2b3f9eaa2b1707b2e1b50f447414f6a765d6e10
osamakhaled123 <osamakhaled132000@gmail.com>
2026-02-04 22:35:23 +0200

    initalizing the training setup

--- src/train.py
+++ src/train.py
@@ -1,1 +1,21 @@
+from huggingface_hub import login
+from _config import token_name
+import torch
 from peft import get_peft_model, LoraConfig
+from trl import SFTTrainer
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, DataCollatorWithPadding
+
+login(token=token_name)
+
+model_name = "meta-llama/Llama-3.2-3B"
+
+lora_config = LoraConfig(
+    r=8,
+    lora_alpha=16,
+    bias="none",
+    lora_dropout=0.1,
+    task_type="Causal_LM",
+    target_modules=["q_proj", "v_proj",  "gate_proj", "up_proj", "down_proj"]
+)
+
+model = AutoModelForCausalLM.from_pretrained(model_name, , device_map="auto")
