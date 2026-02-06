f959ba24215910f950f62645f182fd08e9ac82dc
osamakhaled123 <osamakhaled132000@gmail.com>
2026-02-06 19:48:21 +0200

    updating sharding function

--- src/sharding_hf_transfering.py
+++ src/sharding_hf_transfering.py
@@ -13,14 +13,26 @@ OUTPUT_DIR = ".\jsonl_shards"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
-def sharding(dataset, set, SHARD_SIZE = 50_000):
+def sharding(dataset, set, SHARD_SIZE=50_000):
     buffer = []
     shard_idx = 0
 
     for i, example in enumerate(tqdm.tqdm(dataset)):
         buffer.append(example)
 
-    if len(buffer) == SHARD_SIZE:
+        if len(buffer) == SHARD_SIZE:
+            shard_path = os.path.join(
+                OUTPUT_DIR, f"{set}_{shard_idx:04d}.jsonl"
+            )
+
+            with open(shard_path, "w", encoding="utf-8") as f:
+                for row in buffer:
+                    f.write(json.dumps(row) + "\n")
+
+            buffer.clear()
+            shard_idx += 1
+
+    if buffer:
         shard_path = os.path.join(
             OUTPUT_DIR, f"{set}_{shard_idx:04d}.jsonl"
         )
@@ -28,18 +40,6 @@ def sharding(dataset, set, SHARD_SIZE = 50_000):
         with open(shard_path, "w", encoding="utf-8") as f:
             for row in buffer:
                 f.write(json.dumps(row) + "\n")
-
-        buffer.clear()
-        shard_idx += 1
-
-    if buffer:
-        shard_path = os.path.join(
-            OUTPUT_DIR, f"{set}_{shard_idx:04d}.jsonl"
-        )
-
-    with open(shard_path, "w", encoding="utf-8") as f:
-        for row in buffer:
-            f.write(json.dumps(row) + "\n")
 
 
 sharding(train_data, set="train")
