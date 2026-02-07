94d2e63a91123ba424ad6521f0152adb20b0c77a
osamakhaled123 <osamakhaled132000@gmail.com>
2026-02-07 20:51:23 +0200

    uploading Shards of datasets enabling hf_transfer to hugging face datasets hub repo

--- src/sharding_hf_transfering.py
+++ src/sharding_hf_transfering.py
@@ -2,7 +2,8 @@ import os
 import json
 import tqdm
 import datasets
-from huggingface_hub import upload_folder
+from huggingface_hub import HfApi
+from _config import token_name
 
 
 train_data = datasets.load_from_disk("./data/cnn_train_set")
@@ -13,7 +14,7 @@ OUTPUT_DIR = ".\jsonl_shards"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
-def sharding(dataset, set, SHARD_SIZE=50_000):
+def sharding(dataset, set, SHARD_SIZE=10_000):
     buffer = []
     shard_idx = 0
 
@@ -49,8 +50,13 @@ os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 os.environ["HF_HUB_DISABLE_XET"] = "1"
 
 
-upload_folder(
+api = HfApi()
+
+api.upload_folder(
     repo_id="osamakhaledML9/cnn_tokenized_datasets",
-    folder_path="./jsonl_shards",
+    folder_path="./jsonl_shards/",
     repo_type="dataset",
+    path_in_repo="",
+    commit_message="Updating Shards of datasets enabling hf_transfer",
+    token=token_name
 )
